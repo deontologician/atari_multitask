9010699eb21cc846eb5d59438c2ed79b60c95bbd
Josh Kuhn <deontologician@gmail.com>
2017-01-06 07:46:28 -0800

    Saving and loading params works

--- lib/__init__.py
+++ lib/__init__.py
@@ -33,6 +33,13 @@ def game_name(raw_name):
 GAME_NAMES = [game_name(game) for game in GAMES]
 
 
+def get_random_env(game_choices):
+    game_name = random.choice(game_choices)
+    print 'Going to play {} next'.format(game_name)
+    env = gym.make(game_name)
+    return env
+
+
 class Agent(object):
 
     __metaclass__ = ABCMeta  # ensures subclasses implement everything
@@ -60,124 +67,118 @@ class RandomAgent(Agent):
         return self  # RandomAgent has no state
 
 
-class TestPlan2(object):
+class BenchmarkParms(object):
     def __init__(self,
                  num_folds=5,
                  frame_limit=10000000,
                  max_turns_w_no_reward=10000,
+                 seed=None,
                  ):
         self.num_folds = num_folds
         self.frame_limit = frame_limit
         self.max_turns_w_no_reward = max_turns_w_no_reward
+        self.seed = random.random() if seed is None else seed
 
         games = set(GAME_NAMES)
         fold_size = NUM_GAMES // num_folds
         remainder = NUM_GAMES % num_folds
-        folds = []
+        self.folds = [None] * num_folds
 
         for i in range(num_folds):
             if i < remainder:
                 # distribute the remainder games evenly among the folds
-                folds[i] = random.sample(games, fold_size + 1)
+                self.folds[i] = random.sample(games, fold_size + 1)
             else:
-                folds[i] = random.sample(games, fold_size)
+                self.folds[i] = random.sample(games, fold_size)
+            games -= set(self.folds[i])
 
-        for _ in range(NUM_GAMES - remainder):
-            random.sample(games, fold_size)
-
-
-
-
-class TestPlan(object):
-
-    def __init__(self,
-                 holdout_fraction=0.30,
-                 regimen=1,
-                 max_no_reward_turns=10000):
-        '''Creates a TestPlan for running a set of games.
-
-        `holdout_fraction` is a float for what percent of the overall games to
-            keep out of training to be used as test games.
-        `regimen` is the number of games in a row to play before switching
-            to a new random game from the training set.
-        `max_no_signal_turns` is how many turns to go in a row without any
-            reward before artificially saying the game is "done"
-        '''
-        num_test_games = int(ceil(len(GAME_NAMES) * holdout_fraction))
-        self.test_set = sorted(random.sample(GAME_NAMES, num_test_games))
-        self.training_set = sorted(set(GAME_NAMES) - set(self.test_set))
-        self.regimen = regimen
-        self.max_no_reward_turns = max_no_reward_turns
+        assert(len(games) == 0)
 
     def save(self, filename):
         '''Save the TestPlan to a file'''
         filedata = {
-            'test_set': self.test_set,
-            'training_set': self.training_set,
-            'regimen': self.regimen,
-            'max_no_reward_turns': self.max_no_reward_turns,
+            'folds': self.folds,
+            'seed': self.seed,
+            'frame_limit': self.frame_limit,
+            'max_turns_w_no_reward': self.max_turns_w_no_reward,
         }
         with open(filename, 'w') as savefile:
-            json.dump(savefile, filedata, sort_keys=True, indent=True)
+            json.dump(filedata, savefile, sort_keys=True, indent=True)
 
     @staticmethod
     def load_from_file(filename):
-        '''Load a TestPlan from a file'''
+        '''Load a BenchmarkParms from a file'''
         with open(filename, 'r') as savefile:
-            filedata = json.load(filename)
-            plan = TestPlan()
+            filedata = json.load(savefile)
+            parms = BenchmarkParms()
             # Just overwrite the original fields. A little wasteful but w/e
-            plan.test_set = filedata['test_set']
-            plan.training_set = filedata['training_set']
-            plan.regimen = filedata['regimen']
-            plan.max_no_reward_turns = filedata['max_no_reward_turns']
-        return plan
-
-    def train(self, agent, render=False, max_episodes=-1):
-        '''Training in a loop
-        `agent` is the Agent to use to interact with the game
-        `render` is whether to render game frames in real time
-        `max_episodes` will limit episodes to run
-        '''
-        episodes = 0
-        games_in_a_row = 0
-        env = get_random_env(self.training_set)
-        while max_episodes == -1 or episodes <= max_episodes:
-            done = False
-            reward = 0.0
-            if games_in_a_row >= self.regimen:
-                env = get_random_env(self.training_set)
-                games_in_a_row = 0
-            observation = env.reset()
-            no_reward_for = 0
-
-            while not done and no_reward_for <= self.max_no_reward_turns:
-                if render:
-                    env.render()
-                action = agent(env, observation, reward)
-                observation, reward, done, info = env.step(action)
-                no_reward_for = no_reward_for + 1 if not reward else 0
-
-            agent.episode()
-            # update counts
-            games_in_a_row += 1
-            if episodes > -1:
-                episodes += 1
+            parms.folds = filedata['folds']
+            parms.num_folds = len(parms.folds)
+            parms.frame_limit = filedata['frame_limit']
+            parms.max_turns_w_no_reward = filedata['max_turns_w_no_reward']
+            parms.seed = filedata['seed']
+        return parms
 
 
-def get_random_env(game_choices):
-    game_name = random.choice(game_choices)
-    print 'Going to play {} next'.format(game_name)
-    env = gym.make(game_name)
-    return env
+
+class Benchmark(object):
+    def __init__(self, parms, architecture):
+        self.parms = parms  # BenchmarkParms
+
+    def test_set(self, test_index):
+        return set(self.parms.folds[test_index])
+
+    def training_set(self, test_index):
+        return {x
+                for i, fold in enumerate(self.parms.folds)
+                for x in fold if i != test_index}
+
+    def do_folds(self):
+        for test_fold in range(len(self.parms.folds)):
+            test_set = self.test_set(test_fold)
+            training_set = self.training_set(test_fold)
+
+
+    # def train(self, agent, render=False, max_episodes=-1):
+    #     '''Training in a loop
+    #     `agent` is the Agent to use to interact with the game
+    #     `render` is whether to render game frames in real time
+    #     `max_episodes` will limit episodes to run
+    #     '''
+    #     episodes = 0
+    #     games_in_a_row = 0
+    #     env = get_random_env(self.training_set)
+    #     while max_episodes == -1 or episodes <= max_episodes:
+    #         done = False
+    #         reward = 0.0
+    #         if games_in_a_row >= self.regimen:
+    #             env = get_random_env(self.training_set)
+    #             games_in_a_row = 0
+    #         observation = env.reset()
+    #         no_reward_for = 0
+
+    #         while not done and no_reward_for <= self.max_no_reward_turns:
+    #             if render:
+    #                 env.render()
+    #             action = agent(env, observation, reward)
+    #             observation, reward, done, info = env.step(action)
+    #             no_reward_for = no_reward_for + 1 if not reward else 0
+
+    #         agent.episode()
+    #         # update counts
+    #         games_in_a_row += 1
+    #         if episodes > -1:
+    #             episodes += 1
 
 
 def main():
-    plan = TestPlan(holdout_fraction=0.30, regimen=1)
+    bp = BenchmarkParms()
 
-    agent = RandomAgent()
+    architecture = RandomArchitecture()
 
-    plan.train(agent, render=False)
+    bench = Benchmark(bp, architecture)
+
+    bench.train(render=False)
 
 
 if __name__ == '__main__':
